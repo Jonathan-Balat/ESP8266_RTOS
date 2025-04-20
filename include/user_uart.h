216ce598a4245e099caa883e701dc12bd5753eda
Jonathan Balat <engrjonathanbalat@gmail.com>
2025-04-20 18:37:13 +0800

    minor spacing fix

--- include/user_uart.h
+++ include/user_uart.h
@@ -13,7 +13,7 @@
 #define UART_INTR_ALL (UART_INTR_RXFIFO_TOUT | UART_INTR_RXFIFO_FULL | UART_INTR_TXFIFO_EMPTY | UART_INTR_LINE_STATUS)
 
 /* USER Configuration for UART */
-#define USER_UART_NUM      (UART0)
+#define USER_UART_NUM       (UART0)
 #define USER_BAUD_RATE      (115200)    
 #define USER_DATA_BITS      (UART_WordLength_8b)    
 #define USER_PARITY         (USART_Parity_None) 
